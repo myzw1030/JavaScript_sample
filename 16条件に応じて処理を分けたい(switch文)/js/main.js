ae7b08a27f37eaa0cbc806c4c1343b2b04cadd17
tommy <tmtmtoa@gmail.com>
2021-09-18 14:33:37 +0900

    jsの学習

---
+++ 16条件に応じて処理を分けたい(switch文)/js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+{
+    const myFruit = 'りんご';
+
+    switch(myFruit) {
+        case 'りんご':
+            alert('りんごです');
+            break;
+        case 'みかん':
+            alert('みかんです');
+            break;
+        default:
+            alert('その他です');
+            break;
+    }
+
+}

c5d8c065687087ef5ca3f1edde1d81f2149218dd
tommy <tmtmtoa@gmail.com>
2021-09-20 11:31:21 +0900

    js学習

---
+++ 曜日を取得したい/js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const date = new Date();
+const day = date.getDay();
+const dayList = ['日', '月', '火', '水', '木', '金', '土'];
+const label = dayList[day];
+
+document.querySelector('#log').innerHTML = `今日は${label}曜日です`;
